f462f1f7d9671a5d697767aca7530cd3cbb6728f
smertaydin <81705973+smertaydin@users.noreply.github.com>
2021-06-16 12:05:58 +0300

    Create Testing.java

---
+++ Testing.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+
+
+public class Testing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		HtmlUnitDriver driver=new HtmlUnitDriver(BrowserVersion.FIREFOX_38);
+		/*driver.get("http://localhost:8899/qaenv/");
+		*/
+		driver.get("http://127.0.0.1:8080/testapp/");
+		System.out.println(driver.getCurrentUrl());
+		String expmsg="Hi!";
+		String actmsg=driver.findElement(By.xpath("html/body")).getText();
+		System.out.println("Expected  : "+expmsg);
+		System.out.println("Actual Message   : "+actmsg);
+		if(expmsg.equals(actmsg))
+		{
+			System.out.println("Testing has Passed");
+		}
+		else
+		{
+			System.out.println("Testing has Failed");
+			System.out.println(actmsg);
+		}
+		driver.close();
+	}
+
+	}
